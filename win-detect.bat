65d84370ffda9f207847d47dbfe05eeae37aa783
Ioannis A. Bouhras <ibsoft@users.noreply.github.com>
2023-12-02 08:30:40 +0200

    Update win-detect.bat

--- win-detect.bat
+++ win-detect.bat
@@ -3,14 +3,14 @@ setlocal enabledelayedexpansion
 
 set "model_path=.\models\best-model-20231202.pt"
 set "source_directory=detection-images"
-
+set "output_directory=output"
 
 mkdir %output_directory%
 
-for %%i in (%source_directory%\*.jpg) do (
+for %%i in (%source_directory%\*.jpg %source_directory%\*.png %source_directory%\*.gif) do (
     set "image_file=%%i"
     echo Processing: !image_file!
-    yolo detect predict model=!model_path! source=!image_file! 
+    yolo detect predict model=!model_path! source=!image_file! > %output_directory%\output_%%~ni.txt
 )
 
 echo Batch processing complete.
