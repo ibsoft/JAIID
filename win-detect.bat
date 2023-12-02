3d7c90b3aa6ccba1bac620d7c87cff0ce79a79ff
Ioannis A. Bouhras <ibsoft@users.noreply.github.com>
2023-12-02 08:26:59 +0200

    Update win-detect.bat

--- win-detect.bat
+++ win-detect.bat
@@ -1,1 +1,17 @@
-yolo detect predict model="models\\best.pt" source=".\\detection-videos\\impactTest1.mp4"
+@echo off
+setlocal enabledelayedexpansion
+
+set "model_path=.\models\best-model-20231202.pt"
+set "source_directory=detection-images"
+
+
+mkdir %output_directory%
+
+for %%i in (%source_directory%\*.jpg) do (
+    set "image_file=%%i"
+    echo Processing: !image_file!
+    yolo detect predict model=!model_path! source=!image_file! 
+)
+
+echo Batch processing complete.
+pause
