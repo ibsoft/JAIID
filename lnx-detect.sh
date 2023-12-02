f328f997641dfd6f171e34c3a4c4c78ce754f64f
Ioannis A. Bouhras <ibsoft@users.noreply.github.com>
2023-12-02 08:32:59 +0200

    Update lnx-detect.sh

--- lnx-detect.sh
+++ lnx-detect.sh
@@ -1,1 +1,13 @@
-yolo detect predict model="models/best.pt" source="detect-videos/ImpactTest.mp4"
+#!/bin/bash
+
+model_path="./models/best-model-20231202.pt"
+source_directory="detection-images"
+
+
+for image_file in "$source_directory"/*.jpg "$source_directory"/*.png "$source_directory"/*.gif; do
+    echo "Processing: $image_file"
+    yolo detect predict model="$model_path" source="$image_file"
+done
+
+echo "Batch processing complete."
+
